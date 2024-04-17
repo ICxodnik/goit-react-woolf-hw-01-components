acd84ea8d5e65b163afcbe94869fab4cc3649f8c
Natalia <icxodnik@gmail.com>
2024-04-17 19:52:19 -0400

    initial commit

---
+++ src/components/Profile/index.jsx
@@ -0,0 +1,11 @@
+import 'components/Profile/index.module.css';
+const { Component } = require("react");
+
+class Profile extends Component {
+    render() {
+        return <div className="profile">
+        </div>
+    }
+}
+
+export { Profile }
