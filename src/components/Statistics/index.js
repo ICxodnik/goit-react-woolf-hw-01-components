a3df1a590bb562fac39b0a738e462770e6081219
Natalia <icxodnik@gmail.com>
2024-04-23 03:59:35 -0400

    add statistic styles

--- src/components/Statistics/index.js
+++ src/components/Statistics/index.js
@@ -1,14 +1,15 @@
-import './index.module.css';
+import css from './index.module.css';
+import { getRandomColorCssObject } from "../../services/colorGenerator.js"
 
 const Statistic = ({ title, stats }) => (
-    <section className="statistics">
-        {title && (<h2 className="title">Upload stats</h2>)}
+    <section className={css.statistics}>
+        {title && (<h2 className={css.title}>Upload stats</h2>)}
 
-        <ul className="stat-list">
+        <ul className={css.starList}>
             {stats.map(el => (
-                <li className="item" key={el.id}>
-                    <span className="label">{el.label}</span>
-                    <span className="percentage">{el.percentage}%</span>
+                <li className={css.item} key={el.id} style={getRandomColorCssObject()}>
+                    <span className={css.label}>{el.label}</span>
+                    <span className={css.percentage}>{el.percentage}%</span>
                 </li>
             ))}
         </ul>
