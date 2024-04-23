4faa472c48dfade44175f046ff5fd79d36244510
Natalia <icxodnik@gmail.com>
2024-04-23 03:25:00 -0400

    rewrite profile to function style

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,36 +1,33 @@
 import css from 'components/Profile/index.module.css';
-const { Component } = require("react");
 
-class Profile extends Component {
-    render() {
+const Profile = (props) => {
         return <div className={css.profile}>
             <div className={css.description}>
                 <img
-                    src={this.props.avatar}
+                    src={props.avatar}
                     alt="User avatar"
                     className={css.avatar}
                 />
-                <p className={css.name}>{this.props.username}</p>
-                <p className={css.tag}>@{this.props.tag}</p>
-                <p className={css.location}>{this.props.location}</p>
+                <p className={css.name}>{props.username}</p>
+                <p className={css.tag}>@{props.tag}</p>
+                <p className={css.location}>{props.location}</p>
             </div>
 
             <ul className={css.stats}>
                 <li>
                     <span className={css.label}>Followers</span>
-                    <span className={css.quantity}>{this.props.stats.followers}</span>
+                    <span className={css.quantity}>{props.stats.followers}</span>
                 </li>
                 <li>
                     <span className={css.label}>Views</span>
-                    <span className={css.quantity}>{this.props.stats.views}</span>
+                    <span className={css.quantity}>{props.stats.views}</span>
                 </li>
                 <li>
                     <span className={css.label}>Likes</span>
-                    <span className={css.quantity}>{this.props.stats.likes}</span>
+                    <span className={css.quantity}>{props.stats.likes}</span>
                 </li>
             </ul>
         </div>
-    }
 }
 
 export default Profile;
